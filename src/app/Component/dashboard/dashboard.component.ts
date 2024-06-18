fa7587395d76fcac8470f4cdf1e53994505734d0
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-06-18 19:56:53 +0800

    JC

---
+++ src/app/Component/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { GalleryComponent} from "../gallery/gallery.component";
+import {AppComponent} from "../../app.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [GalleryComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./Component/login/login.component";
+import {RegistrationComponent} from "./Component/registration/registration.component";
+import {DashboardComponent} from "./Component/dashboard/dashboard.component";
+import {GalleryComponent} from "./Component/gallery/gallery.component";
+import {ProfileComponent} from "./Component/profile/profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'profile', component: ProfileComponent}
+
+];
